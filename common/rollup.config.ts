45f434b32f2d0d1bb914e239faf9de27f5cc649d
IIIMADDINIII <martin@elend.name>
2022-11-05 18:19:22 +0100

    Projekt Structure

---
+++ common/rollup.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "rollup";
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from "@rollup/plugin-terser";
+
+export default defineConfig({
+  input: "./src/index.ts",
+  output: [{
+    file: "./dist/index.cjs",
+    format: "commonjs",
+    sourcemap: true,
+  }, {
+    file: "./dist/index.mjs",
+    format: "esm",
+    sourcemap: true,
+  }],
+  plugins: [
+    commonjs(),
+    typescript({ noEmitOnError: true, outputToFilesystem: true }),
+    nodeResolve(),
+    terser(),
+  ],
+});
---
+++ preload/rollup.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "rollup";
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import terser from "@rollup/plugin-terser";
+
+export default defineConfig({
+  input: "./src/index.ts",
+  output: {
+    file: "./dist/index.js",
+    format: "commonjs",
+    sourcemap: true,
+  },
+  external: ["electron"],
+  plugins: [
+    commonjs(),
+    typescript({ noEmitOnError: true, outputToFilesystem: true }),
+    nodeResolve(),
+    terser(),
+  ],
+});
