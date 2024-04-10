bc4dbf30bfd829ea7980e769960618b79a0b79b0
IIIMADDINIII <martin@elend.name>
2024-04-10 10:30:40 +0200

    Improve Type Isolation between packages

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,4 +1,10 @@
 
+type TimerHandler = string | Function;
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/clearTimeout) */
+declare function clearTimeout(id: number | undefined): void;
+/** [MDN Reference](https://developer.mozilla.org/docs/Web/API/setTimeout) */
+declare function setTimeout(handler: TimerHandler, timeout?: number, ...arguments: any[]): number;
+
 /**
  * Returns a Promis wich will resolve after the specified amount of milliseconds.
  * @param ms - time in milliseconds to wait before the promise is resolved.
