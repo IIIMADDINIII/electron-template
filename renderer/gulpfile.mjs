3b0f7257c1134ceec776374b08123afe5eb69a5d
IIIMADDINIII <martin@elend.name>
2024-04-14 22:03:51 +0200

    Integrationg Translations

--- renderer/gulpfile.mjs
+++ renderer/gulpfile.mjs
@@ -1,4 +1,4 @@
 import { tools, tasks } from "@iiimaddiniii/js-build-tool";
 
 
-export const build = tools.exitAfter(tasks.rollup.build({ type: "app", environment: "browser" }));
+export const build = tools.exitAfter(tasks.rollup.build({ type: "app", environment: "browser", terserPlugin: { format: { comments: false } } }));
--- renderer/src/index.ts
+++ renderer/src/index.ts
@@ -1,16 +1,12 @@
 
 import { wait } from "@app/common";
 import { MainElement } from "./mainElement.js";
-import { getLocale, getTargetLocales, initLocalization, readySignalIsUsed, readySignalSend, setLocale } from "./mainInterface.js";
+import { initLocalization, readySignalIsUsed, readySignalSend } from "./mainInterface.js";
 
 
 async function run() {
   readySignalIsUsed();
   await initLocalization();
-  (window as any).getLocale = getLocale;
-  (window as any).setLocale = setLocale;
-  (window as any).getTargetLocales = getTargetLocales;
-
   await wait(1000);
   document.body.appendChild(new MainElement());
   readySignalSend();
