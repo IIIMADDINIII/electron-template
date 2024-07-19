fd7d0c5f7e8d6be4cb3805872f62cdce5d5b8a7a
IIIMADDINIII <martin@elend.name>
2024-07-19 18:30:04 +0200

    Remove unused code in locales gulpfile

--- locales/gulpfile.mjs
+++ locales/gulpfile.mjs
@@ -1,10 +1,4 @@
 import { tools, tasks } from "@iiimaddiniii/js-build-tool";
 
-const targetLocales =
-  (await tools.fs.readdir("./translations", { withFileTypes: true }))
-    .filter((e) => e.isFile() && e.name.endsWith(".xlf"))
-    .map((e) => e.name.slice(0, -4));
-const conf = { inputFiles: ["../**/src/**/*.ts"], targetLocales, output: { language: "ts", outputDir: "./src/" }, };
-
 export const build = tools.exitAfter(
   tasks.buildTranslationPackage());
