612fb01b0b09c1e8f2918bee3d9083b05bb85944
IIIMADDINIII <martin@elend.name>
2025-06-17 22:07:23 +0200

    Accept undefined in isElement

--- renderer/src/base/tools.ts
+++ renderer/src/base/tools.ts
@@ -11,8 +11,9 @@ import { initLocalization, initRemote, readySignalIsUsed, readySignalSend, type 
  * @param tag - the Expected Tag Name the Element should have.
  * @returns boolean indicating if element has the tag.
  */
-export function isElement<T extends keyof HTMLElementTagNameMap>(elem: EventTarget | null, tag: T): elem is HTMLElementTagNameMap[T] {
+export function isElement<T extends keyof HTMLElementTagNameMap>(elem: EventTarget | null | undefined, tag: T): elem is HTMLElementTagNameMap[T] {
   if (elem === null) return false;
+  if (elem === undefined) return false;
   if (!(elem instanceof Element)) return false;
   if (elem.tagName.toLocaleUpperCase() !== tag.toLocaleUpperCase()) return false;
   return true;
